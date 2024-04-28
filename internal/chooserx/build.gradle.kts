e9d49b7a3b31f5f8fb198d9f86576ea26d084066
crigarher <crigarher4@alum.us.es>
2024-04-28 14:19:24 +0200

    internal (needed to work)

---
+++ internal/chooserx/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.firebase.example.internal"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 16
+        targetSdk = 34
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+}
+
+dependencies {
+    api("com.google.android.material:material:1.11.0")
+    api("androidx.recyclerview:recyclerview:1.3.2")
+    api("androidx.constraintlayout:constraintlayout:2.1.4")
+}
